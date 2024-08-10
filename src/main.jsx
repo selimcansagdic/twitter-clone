63cd2276aeb7858960ec0fc150e41f3fdc91a004
selimcan sağdıç <selimcanscs1907@gmail.com>
2024-08-10 13:47:19 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDom from "react-dom/client"
+
+ReactDom.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+
+
+  </React.StrictMode>
+)
+
+  
